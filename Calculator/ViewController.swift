63ff30286cf12fd5a397c7b5d4169d7108b201f8
Kaleb Bataran <kalebb@kalebbataran.local>
2016-02-13 10:08:47 -0500

    Added files

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,19 +7,122 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+	
+	//Outlets
+	@IBOutlet weak var calcDisplay: UILabel!
 
+	var btnSound: AVAudioPlayer!
+	
+	//Properties
+	var leftNumber = ""
+	var rightNumber = ""
+	var prevOperator = ""
+	
 	override func viewDidLoad() {
+		
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		calcDisplay.text = ""
+		
+		//Button sound
+		let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+		let soundURL = NSURL(fileURLWithPath: path!)
+		do {
+			try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+			btnSound.prepareToPlay()
+		} catch let err as NSError {
+			print(err.debugDescription)
+		}
+	}
+	
+	@IBAction func numberPressed(btn: UIButton) {
+		
+		//no leading zeros
+		if rightNumber == "" && btn.tag == 0 {
+			return
+		}
+	
+		rightNumber += "\(btn.tag)"
+		calcDisplay.text = rightNumber
+		playSound()
+		
+	}
+	
+	@IBAction func clearButtonPressed(sender: AnyObject) {
+		
+		playSound()
+		leftNumber = ""
+		rightNumber = ""
+		calcDisplay.text = rightNumber
+		prevOperator = ""
+		
+	}
+	
+	func runLogic(op: String) {
+		
+		playSound()
+		
+		if prevOperator == "" {
+			prevOperator = op
+			leftNumber = rightNumber
+			rightNumber = ""
+			return
+		}
+		
+		if rightNumber != "" {
+			
+			if prevOperator == "/" {
+				leftNumber = "\(Double(leftNumber)! / Double(rightNumber)!)"
+				
+			} else if prevOperator == "*" {
+				leftNumber = "\(Double(leftNumber)! * Double(rightNumber)!)"
+				
+			} else if prevOperator == "+" {
+				leftNumber = "\(Double(leftNumber)! + Double(rightNumber)!)"
+				
+			} else if prevOperator == "-" {
+				leftNumber = "\(Double(leftNumber)! - Double(rightNumber)!)"
+				
+			}
+			
+			calcDisplay.text = leftNumber
+			rightNumber = ""
+			
+		}
+		
+		prevOperator = op
+	}
+	
+	@IBAction func equalsButtonPressed(sender: AnyObject) {
+		runLogic("=")
+	}
+	
+	@IBAction func divideButtonPressed(sender: AnyObject) {
+		runLogic("/")
+	}
+	
+	@IBAction func multiplyButtonPressed(sender: AnyObject) {
+		runLogic("*")
+	}
+	
+	@IBAction func addButtonPressed(sender: AnyObject) {
+		runLogic("+")
+	}
+	
+	@IBAction func	subtractButtonPressed(sender: AnyObject){
+		runLogic("-")
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func playSound(){
+		
+		if btnSound.playing{
+			btnSound.stop()
+		}
+		btnSound.play()
+		
 	}
-
 
 }
 
